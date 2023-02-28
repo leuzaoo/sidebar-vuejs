a6b21a851d39068cc85b470ae324e36aacd122ff
leuzaoo <leuzaoocontato@gmail.com>
2023-02-28 02:05:25 -0300

    update application

--- src/App.vue
+++ src/App.vue
@@ -45,10 +45,10 @@ button {
   main {
     flex: 1 1 0;
     padding: 2rem;
-  }
 
-  @media (max-width: 768px) {
-    padding-left: 6rem;
+    @media (max-width: 768px) {
+      padding-left: 6rem;
+    }
   }
 }
 </style>
